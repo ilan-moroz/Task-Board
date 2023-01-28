845f408ccb6819f4699c5e0527d9884a197f092b
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-01-28 15:06:05 +0200

    remove fix

--- js/myjs.js
+++ js/myjs.js
@@ -39,13 +39,13 @@ const makeTask = () => {
     data += `<div id="newTask" class="fade-in card bg-transparent text-dark">
     <img src="img/notebg.png" class="card-img" alt="card-img-overlay">
     <div class="card-img-overlay ">
-      <span class="text-danger glyphicon glyphicon-remove d-flex justify-content-end" onclick="removeTask()"></span>
+      <span class="text-danger glyphicon glyphicon-remove d-flex justify-content-end" onclick="removeTask(event)"></span>
       <div class="mainTask">
         <p class="card-task">${item.name}</p>
       </div>
       <div class="bottom">
-        <p class="card-text">${item.date}</p>
-        <p class="card-text"> ${item.time}</p>
+        <p class="card-text">${item.time}</p>
+        <p class="card-text"> ${item.date}</p>
       </div>
     </div>
   </div>`
@@ -61,17 +61,19 @@ const checkForData = () => {
 }
 
 // REMOVE TASK
-const removeTask = () => {
+const removeTask = (event) => {
   const remove = document.getElementById('note')
+  const target = event.target
   for (let index = 0; index < remove.children.length; index++) {
-    remove.children[index].onclick = function () {
+    if (remove.children[index].contains(target)) {
       if (index != 0) {
         remove.children[index - 1].classList.remove('fade-in')
+        myTasks.splice(index, 1)
+        remove.removeChild(remove.children[index])
+        localStorage.removeItem[index]
+        localSave()
+        break
       }
-      myTasks.splice(index, 1)
-      remove.removeChild(remove.children[index])
-      localStorage.removeItem[index]
-      localSave()
     }
   }
 }
